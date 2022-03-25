5c8086f6a0d37a8e1d77d9ac003f3223e5e63f2e
Vadim Kozlov <diginet@mail.ru>
2022-03-26 01:35:57 +0700

    minor changes

---
+++ tests/test_figure.py
@@ -0,0 +1,30 @@
+from figures.figure import Figure
+import pytest
+
+test_data =[
+    ((1), 0),
+    ((1,), 1),
+    ((1,2,3), 3),
+    (('1.0','2.0','3.0'), 3),
+    (('l',2,'З'), 0) ]
+
+class TestFigure():
+
+    f = Figure()
+
+    def test_name(self):
+        assert self.f.name == 'A figure'
+
+    @pytest.mark.parametrize('param, expected', test_data)
+    def test_set_dimensions(self, param, expected):
+        self.f._dimensions = ()
+        try:
+            self.f._dimensions = param
+        except:
+            pass
+        assert len(self.f._dimensions) == expected
+    
+    def test_get_dimensions(self):
+        data = (1, '2', 3.333)
+        self.f._dimensions = data
+        assert self.f._dimensions == (1, 2.0, 3.333)
