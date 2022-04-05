0b4b126d965408cb63a194ad57893112063c5e37
Vadim Kozlov <diginet@mai.ru>
2022-04-05 22:39:19 +0700

    further classes development

--- books_example.py
+++ books_example.py
@@ -1,12 +1,5 @@
 from src.books.classes import Book, Person, Reader
+from copy import copy
+import json
 
 
-b = Book()
-print(b.__dict__)
-
-b.author = "John Smith"
-b.genre = "fantasy"
-b.pages = 101
-b.title = "Too hard to die"
-
-print(b.__dict__)
